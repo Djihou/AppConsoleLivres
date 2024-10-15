09b950ccec8c6bcd4488a5edf44ddc86a2af52a8
Hertz Sully Djihoua <300139241@monboreal.ca>
2024-10-15 12:38:10 -0400

    Deuxième version de l'app: Classe mère livre

---
+++ Livre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        public string Titre { get; set; }
+        public string Auteur{ get; set; }
+        public int NombrePages { get; set; }
+
+
+        public Livre (string titre, string auteur, int nombrePage)
+        {  Titre = titre; 
+            Auteur = auteur; 
+            NombrePages = nombrePage;
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Le livre intitulé {Titre} a pour auteur {Auteur} et contient {NombrePages} pages");
+
+        }
+    }
+}
