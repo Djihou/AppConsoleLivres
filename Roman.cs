d4c0dc660edd1b08a412e504ba7c771455eeefd6
Hertz Sully Djihoua <300139241@monboreal.ca>
2024-10-15 13:26:44 -0400

    + Classe fille Roman

---
+++ Roman.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Roman : Livre
+    {
+        public string Genre { get; set; }
+
+        public Roman(string titre, string auteur, int nombrePages, string genre):base(titre, auteur, nombrePages)
+        {
+            Genre = genre;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Le livre intitulé {Titre} a pour auteur {Auteur} et contient {NombrePages} pages.Son genre est {Genre}");
+        }
+    }
+}
